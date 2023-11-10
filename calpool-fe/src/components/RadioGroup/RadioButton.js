c5031429261a4a6ba0bac671b3ce72c526ec7800
Lucas J <lucas@wifi-10-40-179-122.wifi.berkeley.edu>
2023-11-09 21:02:19 -0800

    radio buttons

---
+++ calpool-fe/src/components/RadioGroup/RadioButton.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
+export default function RadioButton({children}) {
+  return (
+        <FormControlLabel value="other" control={<Radio />} label="Other" />
+
+  );
+}
--- calpool-fe/src/pages/InputUserInfo/InputUserInfo.js
+++ calpool-fe/src/pages/InputUserInfo/InputUserInfo.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { TextInput } from '../../components'
+import { handleFormChange } from '../../utils/utils'
 
 const InputUserInfo = () => {
+    const [form, setForm] = useState({
+        phone: { value: "", error: "" },
+        major: { value: "", error: "" },
+        gender: { value: "", error: "" },
+        grad_year: { value: 2023, error: "" },
+    });    
   return (
     <div className="container">
       <h1 className="header">Welcome, {}</h1>
       <form onSubmit={handleSubmit} className="form">
-      <TextInput type='text' error={form.email.error} placeholder="Email" name='email' onChange={handleFormChange} value={form.email.value}/>
-      <TextInput type='password' error={form.password.error} placeholder="Password" name='password' onChange={handleFormChange} value={form.password.value}/>
-      <Button type='submit' color='primary' loading={false}>Sign In</Button>
+      <TextInput type='text' error={form.phone.error} placeholder="Mobile Phone" name='phone' onChange={(event) => handleFormChange(event, setForm)} value={form.phone.value}/>
+      <TextInput type='password' error={form.password.error} placeholder="Password" name='password' onChange={(event) => handleFormChange(event, setForm)} value={form.major.value}/>
+      <Button type='submit' color='primary' loading={false}>Next</Button>
       </form>
       <Link
           href=""
